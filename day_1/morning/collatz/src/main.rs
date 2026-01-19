8d79cf95b849109b82590c18da15397b76b29101
U-lis <ulismoon@gmail.com>
2026-01-20 00:17:58 +0900

    Exercise: collatz length

---
+++ day_1/morning/collatz/src/main.rs
@@ -0,0 +1,11 @@
+/*
+콜라츠 수열 : 양의 정수 N이 주어졌을 때, 이 값이 짝수이면 2로 나누고, 홀수이면 3을 곱하고 1을 더한다. 계산 결과값이 1이 되면 수열이 종료된다.
+*/
+/// `n`에서 시작하는 콜라츠 수열의 길이를 결정합니다.
+fn collatz_length(mut n: i32) -> u32 {
+    todo!("Implement this")
+}
+
+fn main() {
+    todo!("Implement this")
+}
