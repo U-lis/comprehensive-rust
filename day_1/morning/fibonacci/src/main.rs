aba21d19f46e023c47ce54ad33e1cc0aba49cd5b
U-lis <ulismoon@gmail.com>
2026-01-19 23:44:17 +0900

    Exercise: fibonacci

---
+++ day_1/morning/fibonacci/src/main.rs
@@ -0,0 +1,14 @@
+fn fib(n: u32) -> u32 {
+    if n <= 2 {
+        // 기본 사례입니다.
+        todo!("Implement this")
+    } else {
+        // 재귀 사례입니다.
+        todo!("Implement this")
+    }
+}
+
+fn main() {
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
+}
