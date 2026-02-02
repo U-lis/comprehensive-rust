06bd761c8e03b92fa60c054c212e92c034877ab6
U-lis <ulismoon@gmail.com>
2026-02-02 23:55:49 +0900

    Exercise: Transpose matrix

---
+++ day_1/afternoon/nested_arrays/src/main.rs
@@ -0,0 +1,29 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result: [[i32; 3]; 3] = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    result
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("Original:");
+    for row in matrix {
+        println!("{row:?}");
+    }
+
+    let transposed = transpose(matrix);
+
+    println!("\nTransposed:");
+    for row in transposed {
+        println!("{row:?}");
+    }
+}
